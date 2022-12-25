89e8b4533736029604353b09efc52f4ee5d73faa
Betul Alptekin <f16btl@gmail.com>
2022-12-25 16:48:24 +0300

    branch v3

---
+++ src/B3.java
@@ -0,0 +1,21 @@
+public class B3 {
+
+    public static void main(String[] args) {
+
+    }
+
+        /*
+        Projemizde önemli bir değişiklik yapmak istiyoruz
+        bu değişikliğe 10 gün çalışacağız
+        Değişikliği yapmaya devam ederken orijinal projemizi riske atmamak
+        ve eger son kullanıcı kullanıyorsa oluşabilecek sorunları engellemek için,
+
+        orijinal proje üzerinde çalışmak yerine
+        yeni bir branch oluşturup
+        o branch'te değişiklikleri yaparız
+
+        çalışma sonunda istenen hedeflere ulaşabilirsek
+        oluşturduğumuz branchi
+        asıl branche'e kopyalarız
+         */
+}
