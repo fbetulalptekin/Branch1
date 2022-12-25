6373471b46fc5e5e922942e5e0aecb3c1c4c4dbe
Betul Alptekin <f16btl@gmail.com>
2022-12-25 17:09:54 +0300

    çakışma öncesi son versiyon v7

---
+++ src/B7.java
@@ -0,0 +1,13 @@
+public class B7 {
+
+
+    /*
+        -Farklı projelerde çalışırken-
+        Kendi projemiz olduğu için farklı class'larda çalışıp
+        çakışma olmasını engelledik
+
+        ama bazen ister istemez çakışmalar olabilir
+
+         */
+
+}
